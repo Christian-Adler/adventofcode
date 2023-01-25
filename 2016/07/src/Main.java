389323506756abc19e0fa63db12e481087c25485
Christian Adler <chris.adler@gmx.de>
2023-01-25 18:47:42 +0100

    2016 07 part 2

--- 2016/07/src/Main.java
+++ 2016/07/src/Main.java
@@ -4,27 +4,27 @@ import java.nio.file.Files;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 //        runForInput("./input_0.txt");
-    runForInput("./input_1.txt");
-  }
+        runForInput("./input_1.txt");
+    }
 
-  private static void runForInput(String inputFileName) throws IOException {
-    System.out.println("\r\nInput: " + inputFileName);
+    private static void runForInput(String inputFileName) throws IOException {
+        System.out.println("\r\nInput: " + inputFileName);
 
-    Task task = new Task();
+        Task task = new Task();
 //        Task2 task = new Task2();
-    task.init();
+        task.init();
 
-    long t1 = System.currentTimeMillis();
-    Files.lines(new File(inputFileName).toPath()).forEach(task::addLine);
+        long t1 = System.currentTimeMillis();
+        Files.lines(new File(inputFileName).toPath()).forEach(task::addLine);
 
-    task.afterParse();
-    long t2 = System.currentTimeMillis();
+        task.afterParse();
+        long t2 = System.currentTimeMillis();
 
 //        System.out.println(task);
-    System.out.println("Duration: " + (t2 - t1) + "ms");
-  }
+        System.out.println("Duration: " + (t2 - t1) + "ms");
+    }
 
 
 }
--- 2016/07/src/Task.java
+++ 2016/07/src/Task.java
@@ -1,65 +1,107 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
 
-  long abbaCounter = 0;
+    long abbaCounter = 0;
+    long sslCounter = 0;
 
-  public void init() {
-  }
-
-  public void addLine(String input) {
-    String[] parts = input.split("[\\[\\]]");
-    boolean isAbbaEven = false;
-    boolean isAbbaOdd = false;
-    for (int i = 0; i < parts.length; i++) {
-      String part = parts[i];
-      boolean isPartAbba = isAbba(part);
-      if (i % 2 == 0) {
-        isAbbaEven |= isPartAbba;
-      } else {
-        isAbbaOdd |= isPartAbba;
-      }
-    }
-    if (isAbbaEven && !isAbbaOdd) {
-      abbaCounter++;
-    }
-  }
-
-  public void afterParse() {
-    out("abbaCount:", abbaCounter);
-  }
-
-
-  private boolean isAbba(String input) {
-    if (input.length() < 4) {
-      return false;
+    public void init() {
     }
 
-    char c0 = input.charAt(0);
-    char c1 = input.charAt(1);
-    char c2 = input.charAt(2);
+    public void addLine(String input) {
+        String[] parts = input.split("[\\[\\]]");
+        boolean isAbbaEven = false;
+        boolean isAbbaOdd = false;
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            boolean isPartAbba = isAbba(part);
+            if (i % 2 == 0) {
+                isAbbaEven |= isPartAbba;
+            } else {
+                isAbbaOdd |= isPartAbba;
+            }
+        }
+        if (isAbbaEven && !isAbbaOdd) {
+            abbaCounter++;
+        }
 
-    for (int i = 3; i < input.length(); i++) {
-      char c = input.charAt(i);
-
-      if (c0 == c && c1 == c2 && c0 != c1) {
-        return true;
-      }
-
-      c0 = c1;
-      c1 = c2;
-      c2 = c;
+        // Part 2 ssl
+        ArrayList<SSLPair> sslPairs = new ArrayList<>();
+        for (int i = 0; i < parts.length; i++) {
+            if (i % 2 != 0) continue;
+            String part = parts[i];
+            sslPairs.addAll(collectABA(part));
+        }
+        out(sslPairs);
+        for (int i = 0; i < parts.length; i++) {
+            if (i % 2 == 0) continue;
+            String part = parts[i];
+            for (SSLPair sslPair : sslPairs) {
+                if (part.contains(sslPair.bab)) {
+                    sslCounter++;
+                    return; // in dieser IP nicht weiter suchen - es gibt bei einigen noch weitere Treffer - aber jede IP zaehlt ja nur einmal
+                }
+            }
+        }
     }
-    return false;
-  }
 
-  public void out(Object... str) {
-    Util.out(str);
-  }
+    public void afterParse() {
+        out("abbaCount:", abbaCounter);
+        out("sslCount:", sslCounter);
+    }
 
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
 
-    return builder.toString();
-  }
+    private boolean isAbba(String input) {
+        if (input.length() < 4) {
+            return false;
+        }
+
+        char c0 = input.charAt(0);
+        char c1 = input.charAt(1);
+        char c2 = input.charAt(2);
+
+        for (int i = 3; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c0 == c && c1 == c2 && c0 != c1) {
+                return true;
+            }
+
+            c0 = c1;
+            c1 = c2;
+            c2 = c;
+        }
+        return false;
+    }
+
+    private List<SSLPair> collectABA(String input) {
+        List<SSLPair> res = new ArrayList<>();
+        if (input.length() < 3) {
+            return res;
+        }
+
+        char c0 = input.charAt(0);
+        char c1 = input.charAt(1);
+
+        for (int i = 2; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c0 == c && c0 != c1) {
+                res.add(new SSLPair("" + c + c1 + c, "" + c1 + c + c1));
+            }
+
+            c0 = c1;
+            c1 = c;
+        }
+        return res;
+    }
+
+    public void out(Object... str) {
+        Util.out(str);
+    }
+
+    private record SSLPair(String aba, String bab) {
+    }
 
 }
