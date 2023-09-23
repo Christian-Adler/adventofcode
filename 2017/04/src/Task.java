5290105409ad8fe6408b26cb0051a5870d07c174
Christian Adler <chris.adler@gmx.de>
2023-09-23 20:29:26 +0200

    2017 04

---
+++ 2017/04/src/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+    public void init() {
+    }
+
+    public void addLine(String input) {
+    }
+
+    public void afterParse() {
+    }
+
+    public void out(Object... str) {
+        Util.out(str);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        return builder.toString();
+    }
+}
