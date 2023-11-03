e82319febd83e7a42ed72e98d8d9897ef74bf493
Christian Adler <chris.adler@gmx.de>
2023-11-03 12:01:04 +0100

    add write to file util

--- template_java/src/Util.java
+++ template_java/src/Util.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,10 +21,21 @@ public class Util {
     public static void out(Object... str) {
         StringBuilder out = new StringBuilder();
         for (Object o : str) {
-            if (out.length() > 0)
+            if (!out.isEmpty())
                 out.append(" ");
             out.append(o);
         }
         System.out.println(out);
+    }
+
+    public static void writeToFile(String content, String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        byte[] strToBytes = content.getBytes();
+
+        Files.write(path, strToBytes);
+    }
+
+    public static void writeToAOCSvg(String content) throws IOException {
+        writeToFile(content, "C:\\Users\\chris\\Downloads\\aoc.svg");
     }
 }
