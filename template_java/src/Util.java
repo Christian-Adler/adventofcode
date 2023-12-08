df9a924a750bc8e7baa4b443dfe023b1c99c5c80
Christian Adler <chris.adler@gmx.de>
2023-12-08 21:03:11 +0100

    add kgv calculation

--- template_java/src/Util.java
+++ template_java/src/Util.java
@@ -38,4 +38,23 @@ public class Util {
     public static void writeToAOCSvg(String content) throws IOException {
         writeToFile(content, "C:\\Users\\chris\\Downloads\\aoc.svg");
     }
+	
+	public static long kgV(long... values) {
+    if (values.length == 0) return 0;
+    if (values.length == 1) return values[0];
+
+    long maxVal = Arrays.stream(values).max().orElse(-1);
+    long kgv = maxVal;
+    while (!allDivisible(kgv, values))
+      kgv += maxVal;
+    return kgv;
+  }
+
+  private static boolean allDivisible(long testValue, long[] values) {
+    for (long val : values) {
+      if (testValue % val != 0)
+        return false;
+    }
+    return true;
+  }
 }
