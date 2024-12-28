c76e9a0640dfe98f7fc258d80bd9b7796408139c
Christian Adler <chris.adler@gmx.de>
2024-12-28 09:45:29 +0100

    2021 10 improve

--- 2021/10/src/Main.java
+++ 2021/10/src/Main.java
@@ -3,15 +3,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hallo");
-        Lines lines = new Lines();
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hallo");
+    Lines lines = new Lines();
 
-        Files.lines(new File("D:/java/adventofcode/10/input1.txt").toPath()).forEach(lines::add);
+    Files.lines(new File("./input1.txt").toPath()).forEach(lines::add);
 
-        System.out.println(lines);
-        System.out.println("ErrScore: " + lines.errScore());
-        System.out.println("CompleteScore: " + lines.completeScore());
+    System.out.println(lines);
+    System.out.println("ErrScore: " + lines.errScore());
+    System.out.println("CompleteScore: " + lines.completeScore());
 
-    }
+  }
 }
