f8be9f63287cad36bf5651621149157a61848a7c
Christian Adler <chris.adler@gmx.de>
2024-12-28 09:48:47 +0100

    2021 11 improve

--- 2021/11/src/Main.java
+++ 2021/11/src/Main.java
@@ -4,41 +4,41 @@ import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hallo");
-        Cave cave = new Cave();
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hallo");
+    Cave cave = new Cave();
 
-        AtomicInteger lineNo = new AtomicInteger();
-        Files.lines(new File("D:/java/adventofcode/11/input.txt").toPath()).forEach((line) ->
-        {
-            String[] split = line.split("");
-            for (int i = 0; i < split.length; i++) {
-                int e = Integer.parseInt(split[i], 10);
-                Position pos = new Position(i, lineNo.get());
-                Octo octo = new Octo(e, pos);
-                cave.setOcto(octo);
-            }
+    AtomicInteger lineNo = new AtomicInteger();
+    Files.lines(new File("./input.txt").toPath()).forEach((line) ->
+    {
+      String[] split = line.split("");
+      for (int i = 0; i < split.length; i++) {
+        int e = Integer.parseInt(split[i], 10);
+        Position pos = new Position(i, lineNo.get());
+        Octo octo = new Octo(e, pos);
+        cave.setOcto(octo);
+      }
 
-            lineNo.getAndIncrement();
-        });
+      lineNo.getAndIncrement();
+    });
 
-        System.out.println(cave);
+    System.out.println(cave);
 
 //        for (int i = 1; i < 200; i++) {
 //            if (cave.step())
 //                System.out.println("All flashed at step: " + i);
 //        }
 
-        int step = 1;
-        while (!cave.step()) {
-            step++;
-        }
-        System.out.println("All flashed at step: " + step);
-
-
-        System.out.println(cave);
-        System.out.println("Flashes: " + Octo.flashes);
-
-
+    int step = 1;
+    while (!cave.step()) {
+      step++;
     }
+    System.out.println("All flashed at step: " + step);
+
+
+    System.out.println(cave);
+    System.out.println("Flashes: " + Octo.flashes);
+
+
+  }
 }
