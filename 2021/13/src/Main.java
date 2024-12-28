c405662f5ac97aebaa8155bdbdb5c7acaca38bf4
Christian Adler <chris.adler@gmx.de>
2024-12-28 09:51:23 +0100

    2021 13 improve

--- 2021/13/src/Main.java
+++ 2021/13/src/Main.java
@@ -3,29 +3,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hallo");
-        Transparent transparent = new Transparent();
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hallo");
+    Transparent transparent = new Transparent();
 
-        Files.lines(new File("D:/java/adventofcode/13/input1.txt").toPath()).forEach((line) -> {
-                    if (line.contains(","))
-                        transparent.add(new Pos(line));
-                    else if (line.startsWith("fold along")) {
-                        System.out.println("\r\nBefore fold");
+    Files.lines(new File("./input1.txt").toPath()).forEach((line) -> {
+          if (line.contains(","))
+            transparent.add(new Pos(line));
+          else if (line.startsWith("fold along")) {
+            System.out.println("\r\nBefore fold");
 //                        System.out.println(transparent);
-                        System.out.println("Size: " + transparent.size());
-                        transparent.fold(line);
-                        System.out.println("\r\nAfter fold");
+            System.out.println("Size: " + transparent.size());
+            transparent.fold(line);
+            System.out.println("\r\nAfter fold");
 //                        System.out.println(transparent);
-                        System.out.println("Size: " + transparent.size());
-                        System.out.println("\r\n");
-                    }
-                }
+            System.out.println("Size: " + transparent.size());
+            System.out.println("\r\n");
+          }
+        }
 
-        );
+    );
 
-        System.out.println(transparent);
-        System.out.println("Size: " + transparent.size());
+    System.out.println(transparent);
+    System.out.println("Size: " + transparent.size());
 // CEJKLUGJ
-    }
+  }
 }
