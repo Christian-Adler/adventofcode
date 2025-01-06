6f41bae379caccd832c83b35e868fe30081879b7
Christian Adler <chris.adler@gmx.de>
2025-01-06 07:11:27 +0100

    2019 09 add computation for day 17 input

--- 2019/09/src/Main.java
+++ 2019/09/src/Main.java
@@ -3,32 +3,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) throws IOException, InterruptedException {
 //        runForInput("./input_0.txt");
 //        runForInput("./input_0_1.txt");
 //        runForInput("./input_0_2.txt");
 //        runForInput("./input_0_3.txt");
-        runForInput("./input_1.txt");
-    }
+    runForInput("./input_1.txt");
+    // runForInput("./input_day17.txt");
+  }
 
-    private static void runForInput(String inputFileName) throws IOException, InterruptedException {
-        System.out.println("\r\nInput: " + inputFileName);
+  private static void runForInput(String inputFileName) throws IOException, InterruptedException {
+    System.out.println("\r\nInput: " + inputFileName);
 
-        Task task = new Task();
-//        Task2 task = new Task2();
-        task.init();
+    Task task = new Task();
+    task.init();
 
-        Files.lines(new File(inputFileName).toPath()).forEach(task::addLine);
+    Files.lines(new File(inputFileName).toPath()).forEach(task::addLine);
 
-        long t1 = System.currentTimeMillis();
-        task.afterParse();
-        long t2 = System.currentTimeMillis();
+    long t1 = System.currentTimeMillis();
+    if (inputFileName.contains("day17"))
+      task.afterParse_Day17();
+    else
+      task.afterParse();
+    long t2 = System.currentTimeMillis();
 
 //        System.out.println(task);
 //        System.out.println(task.toStringSVG());		
 //        System.out.println(task.toStringConsole());
-        System.out.println("Duration: " + (t2 - t1) + "ms");
-    }
+    System.out.println("Duration: " + (t2 - t1) + "ms");
+  }
 
 
 }
--- 2019/09/src/Task.java
+++ 2019/09/src/Task.java
@@ -4,28 +4,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task {
-    Map<Long, Long> programInitial = new HashMap<>();
+  Map<Long, Long> programInitial = new HashMap<>();
 
-    public void init() {
-    }
+  public void init() {
+  }
 
-    public void addLine(String input) {
-        programInitial = Computer.parseProgram(input);
-    }
+  public void addLine(String input) {
+    programInitial = Computer.parseProgram(input);
+  }
 
-    public void afterParse() throws InterruptedException {
-        Computer computer = new Computer(programInitial, value -> out("Part 1", "Output", value));
-        computer.addInput(1L);
-        computer.exec();
-        computer.join();
+  public void afterParse() throws InterruptedException {
+    Computer computer = new Computer(programInitial, value -> out("Part 1", "Output", value));
+    computer.addInput(1L);
+    computer.exec();
+    computer.join();
 
-        computer = new Computer(programInitial, value -> out("Part 2", "Output", value));
-        computer.addInput(2L);
-        computer.exec();
-        computer.join();
-    }
+    computer = new Computer(programInitial, value -> out("Part 2", "Output", value));
+    computer.addInput(2L);
+    computer.exec();
+    computer.join();
+  }
 
-    public void out(Object... str) {
-        Util.out(str);
-    }
+  public void afterParse_Day17() throws InterruptedException {
+    StringBuilder builder = new StringBuilder();
+
+    Computer computer = new Computer(programInitial, value -> {
+      if (value == 35)
+        builder.append("#");
+      else if (value == 46)
+        builder.append(".");
+      else if (value == 10)
+        builder.append("\r\n");
+      else
+        builder.append(">");
+    });
+    computer.exec();
+    computer.join();
+
+    out("Day 17 Input", "\r\n" + builder);
+  }
+
+  public void out(Object... str) {
+    Util.out(str);
+  }
 }
