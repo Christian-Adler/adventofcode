9d4b345cf8248de16635afd11cd69a8cfb16aff7
Christian Adler <chris.adler@gmx.de>
2025-01-06 06:53:05 +0100

    2019 16 part 2

--- 2019/16/src/Task.java
+++ 2019/16/src/Task.java
@@ -9,8 +9,9 @@ public class Task {
   }
 
   public void addLine(String input) {
-    List<Integer> inputList = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).boxed().toList();
+    List<Integer> inputL = Arrays.stream(input.split("")).mapToInt(Integer::parseInt).boxed().toList();
 
+    ArrayList<Integer> inputList = new ArrayList<>(inputL);
     List<Integer> outputList = new ArrayList<>();
 
     for (int i = 0; i < 100; i++) {
@@ -21,7 +22,29 @@ public class Task {
     }
 
     // out(outputList);
-    out(outputList.subList(0, 8).stream().map(Object::toString).collect(Collectors.joining()));
+    out("part 1", outputList.subList(0, 8).stream().map(Object::toString).collect(Collectors.joining()));
+
+    // part 2
+    // didn't understand the hint with the offset - see https://todd.ginsberg.com/post/advent-of-code/2019/day16/
+    // because offset is larger than half of the stretched input we don't have to care about the numbers before.
+    // The second half of input is for each no *1
+    Integer offset = inputL.subList(0, 7).stream().reduce((integer, integer2) -> integer * 10 + integer2).orElseThrow();
+    // out(offset);
+    // out(inputL.size() * 10000);
+    ArrayList<Integer> stretchedInput = new ArrayList<>();
+    for (int i = offset; i < 10000 * inputL.size(); i++) {
+      stretchedInput.add(inputL.get(i % inputL.size()));
+    }
+    // out(stretchedInput.size());
+
+    for (int j = 0; j < 100; j++) {
+      int sumMod10 = 0;
+      for (int i = stretchedInput.size() - 1; i >= 0; i--) {
+        sumMod10 = (sumMod10 + stretchedInput.get(i)) % 10;
+        stretchedInput.set(i, sumMod10);
+      }
+    }
+    out("part 2", stretchedInput.subList(0, 8).stream().map(Object::toString).collect(Collectors.joining()));
   }
 
   private void ttf(List<Integer> inputList, List<Integer> outputList) {
